03d1e6babe41139c44beabbdb94bc5618b14f0cf
= <=>
2023-12-05 21:00:49 +0500

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+#  Ввести строку и образец поиска, вывести только слова, заканчивающиеся на образец поиска
+while True:
+    stroka = input('Введите строку: ')
+    stroka1 = stroka.split()
+    poisk = input('Введите образец поиска: ')
+
+    for word in stroka1:
+        word = list(word)
+        a = word[-len(poisk):]
+        b = ''.join(a)
+        c = ''.join(word)
+        if b == poisk:
+            print(c)
